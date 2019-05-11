16a1037ff29f5bea9ed2d7e99edbe0ee9bf1e20c
Kieran <3812864+MDCEY@users.noreply.github.com>
2019-05-11 11:04:50 +0100

    initial commit. bare bones

--- rimoto_plex_web/rimoto_plex_web.py
+++ rimoto_plex_web/rimoto_plex_web.py
@@ -1,11 +1,21 @@
 
-from flask import Flask
-from flask_socketio import SocketIO
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rimotot.sqlite'
-app.config['SECRET_KEY'] = 'secret!'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rimototo.db'
+#app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 db = SQLAlchemy(app)
 
+@app.route('/')
+def index():
+    return render_template('index.html', port=5123, domain='127.0.0.1')
+
+
+@socketio.on('my_ping')
+def ping_pong():
+    emit('my_pong')
+
+socketio.run(app,port=5123, debug=True)
